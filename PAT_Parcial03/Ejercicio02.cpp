9bb0b241a457dca98b0385334fbe35a57153ebc3
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-25 17:23:35 -0600

    2}¿

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -4,39 +4,31 @@
 
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 { 
-    unordered_map<std::string, int> map;
-    vector<string> resultb = new vector <string>();
-
-    Node<char>* actual = head;
-    while (actual) 
-    {
-        Node<char>* tempo = actual;
-        std::string secuencia = "";
-        for (int i = 0; i < 10 && tempo; i++)
-        {
-            secuencia += tempo->value;
-            tempo = tempo->next;
-        }
-        if (secuencia.size() == 10) 
-        {
-            if (map.find(secuencia) == map.end())
-            {
-                map[secuencia] = 1;
-            } else 
-            {
-                map[secuencia]++;
-            }
-        }
-        actual = actual->next;
-    }
-
-    for (const auto& st : map) 
-    {
-        if (st.second > 1)
-        {
-            result.push_back(st.first);
-        }
-    }
-
-    return result;
+   unordered_map<string, int> map;
+	vector<string>* result = new vector<string>();
+	Node<char>* actual = head;
+	while (actual) {
+		Node<char>* tempo = actual;
+		string secuencia = "";
+		for (int i = 0; i < 10 && tempo; i++) {
+			secuencia += tempo->value;
+			tempo = tempo->next;
+		}
+		if (secuencia.size() == 10) {
+			if (map.find(secuencia) == map.end())
+			{
+				map[secuencia] = 1;
+			}
+			else {
+				map[secuencia]++;
+			}
+		}
+		actual = actual->next;
+	}
+	for (const auto& st : map) {
+		if (st.second > 1) {
+			result->push_back(st.first);
+		}
+	}
+	return result;
 }
