4066ec6b66d18b32171d51c2a908d07bb96354b4
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-25 17:54:05 -0600

    Update Ejercicio02.cpp

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,13 +1,15 @@
 #include "Ejercicio02.h"
 #include <unordered_map>
 #include <vector>
+using namespace std;
 
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 { 
    unordered_map<string, int> map;
 	vector<string>* result = new vector<string>();
 	Node<char>* actual = head;
-	while (actual) {
+	while (actual) 
+	{
 		Node<char>* tempo = actual;
 		string secuencia = "";
 		for (int i = 0; i < 10 && tempo; i++) {
