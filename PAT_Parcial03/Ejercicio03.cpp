f9c9e5b51dbf17543cfe5a25f19cea32aaa2c256
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-25 21:01:25 -0600

    Update Ejercicio03.cpp

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -1,22 +1,16 @@
 #include "Ejercicio03.h"
 
-TimeMap::TimeMap() {}
+TimeMap::TimeMap() 
+{
+	key = "";
+	value = "";
+	time = 0;
+}
 
 void TimeMap::set(string key, string value, int timestamp)
 {
-	data[key][timestamp] = value;
 }
 
 string TimeMap::get(string key, int timestamp)
 {
-	rif (data.find(key) != data.end())
-	{
-	        auto it = data[key].upper_bound(timestamp);
-	        if (it != data[key].begin())
-		{
-	            --it;
-	            return it->second;
-		}
-	}
-    return "";
 }
