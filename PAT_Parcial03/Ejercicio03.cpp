b14c627a4ca5369651e3b8f638f5c4a6ff2f3f8a
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-25 20:45:28 -0600

    Update Ejercicio03.cpp

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -1,40 +1,14 @@
 #include "Ejercicio03.h"
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <map>
 
-using namespace std;
+TimeMap::TimeMap()
+{
+}
 
-class TimeMap {
-private:
-    unordered_map<string, map<int, string>> timeMap;
+void TimeMap::set(string key, string value, int timestamp)
+{
+}
 
-public:
-    TimeMap()
-    {
-    }
-
-    void set(string key, string value, int timestamp)
-    {
-        timeMap[key][timestamp] = value;
-    }
-
-    string get(string key, int timestamp)
-    {
-        auto keyIter = timeMap.find(key);
-        if (keyIter == timeMap.end())
-        {
-            return "";
-        }
-
-        auto timestampIter = keyIter->second.lower_bound(timestamp);
-        if (timestampIter == keyIter->second.begin())
-        {
-            return "";
-        }
-
-        timestampIter--;
-        return timestampIter->second;
-    }
-};
+string TimeMap::get(string key, int timestamp)
+{
+	return "";
+}
