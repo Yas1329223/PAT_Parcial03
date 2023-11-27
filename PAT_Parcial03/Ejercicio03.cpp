3f00546cc9ec0032debbc87d8315a20115f5724e
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-26 19:54:49 -0600

    Update Ejercicio03.cpp

--- PAT_Parcial03/Ejercicio03.cpp
+++ PAT_Parcial03/Ejercicio03.cpp
@@ -4,9 +4,10 @@
 #include <map>
 using namespace std;
 
-TimeMap::TimeMap() 
+TimeMap::TimeMap()
 {
 }
+
 void TimeMap::set(string key, string value, int timestamp)
 {
     map[key].emplace_back(valor{ timestamp, value });
@@ -14,27 +15,30 @@ void TimeMap::set(string key, string value, int timestamp)
 
 string TimeMap::get(string key, int timestamp)
 {
-  if (map.find(key) == map.end())
+    if (map.find(key) == map.end())
         return "";
 
-    vector<Valor>& valores = map[key];
+    vector<valor>& valores = map[key];
 
-    int inicio = 0, medio= 0, fin = valores.size() ;
+    int inicio = 0, medio = 0, fin = valores.size();
 
     if (valores[inicio].timestamp > timestamp)
         return "";
-    while (inicio < fin) 
-    {
+
+    if (valores[fin - 1].timestamp <= timestamp)
+        return valores[fin - 1].value;
+
+    while (inicio < fin) {
         medio = (fin + inicio) >> 1;
 
-        if (valores [medio].timestamp == timestamp)
+        if (valores[medio].timestamp == timestamp)
             return valores[medio].value;
-        
+
         if (valores[medio].timestamp < timestamp)
             inicio = medio + 1;
         else
             fin = medio;
     }
+
     return valores[medio].value;
 }
-
