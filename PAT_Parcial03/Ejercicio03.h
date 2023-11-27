e12518304c86f59bf71f1fc4987ae67352a38bc6
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-26 19:55:14 -0600

    Update Ejercicio03.h

--- PAT_Parcial03/Ejercicio03.h
+++ PAT_Parcial03/Ejercicio03.h
@@ -5,20 +5,22 @@
 #include <map>
 
 using namespace std;
+
+
 using std::string;
 class TimeMap
 {
 private:
-	struct Valor {
-	    int timestamp;
-	    string value;
+	struct valor {
+		int timestamp;
+		string value;
 	};
-    	unordered_map<string, vector<Valor>> map;
+	unordered_map<string, vector<valor>> map;
 public:
 	TimeMap();
 
 	void set(string key, string value, int timestamp);
 
 	string get(string key, int timestamp);
-};
 
+};
